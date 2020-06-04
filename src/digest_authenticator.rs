30ffdc20cd194cd2dd37e0845cfb8e47f0867556
David Webb <david@dpwlabs.com>
2020-06-05 01:29:06 +0930

    Switch back to custom parser for performance
    Compiling the Regex for each parse is a high overhead.
    
    Known bug: not correctly identifying all keys.

--- src/digest_authenticator.rs
+++ src/digest_authenticator.rs
@@ -4,7 +4,6 @@ use http::HeaderMap;
 use md5::Md5;
 use rand::distributions::{Distribution, Uniform};
 use rand::thread_rng;
-use regex::Regex;
 use sha2::Sha256;
 use std::fmt;
 use std::str::FromStr;
@@ -81,7 +80,7 @@ impl fmt::Display for DigestParseError {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
 struct StrPosition {
     start: usize,
     end: usize,
@@ -124,6 +123,7 @@ impl FromStr for DigestScheme {
         if &auth[..6].to_lowercase() != "digest" {
             return Err(DigestParseError::new(DigestParseErrorKind::MissingDigest));
         }
+
         let mut res = Self {
             www_authenticate: auth.to_owned(),
             nonce: StrPosition::default(),
@@ -135,60 +135,78 @@ impl FromStr for DigestScheme {
             algorithm: DigestAlgorithm::MD5,
             session: false,
             userhash: false,
+
             qop: QualityOfProtection::None,
             qop_data: None,
         };
 
-        let matcher = Regex::new(r#"(\S+)\s*=\s*(?:"([^"]+)"|(\S+))\s*(?:,\s*|$)"#).unwrap();
-        for cap in matcher.captures_iter(auth) {
-            let key = &cap[1].to_lowercase();
-            let value = if let Some(v) = cap.get(2) {
-                v
-            } else if let Some(v) = cap.get(3) {
-                v
-            } else {
-                // This isn't valid and should never happen
-                continue;
-            };
-            if key == "nonce" {
-                res.nonce = StrPosition {
-                    start: value.start(),
-                    end: value.end(),
-                };
-            } else if key == "realm" {
-                res.realm = StrPosition {
-                    start: value.start(),
-                    end: value.end(),
-                };
-            } else if key == "domain" {
-            	// @todo solve splitting
-                // res.domain = Some(value.as_str().split(' ').collect());
-            } else if key == "opaque" {
-                res.opaque = StrPosition {
-                    start: value.start(),
-                    end: value.end(),
-                };
-            } else if key == "stale" && value.as_str().to_lowercase() == "true" {
-                res.stale = true;
-            } else if key == "algorithm" {
-                let alg_str = value.as_str().to_lowercase();
-                if alg_str == "md5-sess" {
-                    res.session = true;
-                } else if alg_str == "sha-256" {
-                    res.algorithm = DigestAlgorithm::SHA256;
-                } else if alg_str == "sha-256-sess" {
-                    res.algorithm = DigestAlgorithm::SHA256;
-                    res.session = true;
+        #[derive(PartialEq)]
+        enum KeyVal {
+            PreKey,
+            Key,
+            PreVal,
+            QuoteVal,
+            Val,
+        }
+
+        let mut state = KeyVal::PreKey;
+        let mut key: String = "".to_owned();
+        let mut k_pos = StrPosition::default();
+        let mut v_pos = StrPosition::default();
+
+        for ch_ind in auth.char_indices().skip(6) {
+            match state {
+                KeyVal::PreKey => {
+                    if !ch_ind.1.is_ascii_whitespace() {
+                        k_pos.start = ch_ind.0;
+                        state = KeyVal::Key;
+                    }
                 }
-            } else if key == "qop" {
-                let qop_str = value.as_str().to_lowercase();
-                if qop_str.contains(QualityOfProtection::AuthInt.to_str()) {
-                    res.qop = QualityOfProtection::AuthInt;
-                } else {
-                    res.qop = QualityOfProtection::Auth;
+                KeyVal::Key => {
+                    if ch_ind.1 != '=' {
+                        if !ch_ind.1.is_ascii_whitespace() {
+                            k_pos.end = ch_ind.0 + 1;
+                        }
+                        continue;
+                    }
+                    // ignore '='
+                    key = k_pos.to_str(auth).to_lowercase();
+                    state = KeyVal::PreVal;
                 }
-            } else if key == "userhash" && value.as_str().to_lowercase() == "true" {
-                res.userhash = true;
+                KeyVal::PreVal => {
+                    if !ch_ind.1.is_ascii_whitespace() {
+                        if ch_ind.1 == '"' {
+                            v_pos.start = ch_ind.0 + 1;
+                            state = KeyVal::QuoteVal;
+                        } else {
+                            v_pos.start = ch_ind.0;
+                            state = KeyVal::Key;
+                        }
+                    }
+                }
+                KeyVal::QuoteVal => {
+                    if ch_ind.1 != '"' {
+                        continue;
+                    }
+                    v_pos.end = ch_ind.0;
+                    state = KeyVal::Val;
+                    res.process_header_value(&key, auth, v_pos);
+                }
+                KeyVal::Val => {
+                    if ch_ind.0 != auth.len() - 1 && ch_ind.1 != ',' {
+                        if v_pos.end == 0 && ch_ind.1.is_ascii_whitespace() {
+                            v_pos.end = ch_ind.0;
+                            res.process_header_value(&key, auth, v_pos);
+                        }
+                        continue;
+                    }
+                    if v_pos.end == 0 {
+                        v_pos.end = ch_ind.0;
+                        res.process_header_value(&key, auth, v_pos);
+                    }
+                    v_pos.end = 0;
+                    state = KeyVal::PreKey;
+                }
             }
         }
 
@@ -292,11 +310,45 @@ impl DigestScheme {
         auth
     }
 
+    fn process_header_value(&mut self, key: &str, auth: &str, val_pos: StrPosition) {
+        if key == "nonce" {
+            self.nonce = val_pos;
+        } else if key == "realm" {
+            self.realm = val_pos;
+        } else if key == "domain" {
+            // @todo solve splitting
+            // res.domain = Some(value.as_str().split(' ').collect());
+        } else if key == "opaque" {
+            self.opaque = val_pos;
+        } else if key == "stale" && val_pos.to_str(auth).to_lowercase() == "true" {
+            self.stale = true;
+        } else if key == "algorithm" {
+            let alg_str = val_pos.to_str(auth).to_lowercase();
+            if alg_str == "md5-sess" {
+                self.session = true;
+            } else if alg_str == "sha-256" {
+                self.algorithm = DigestAlgorithm::SHA256;
+            } else if alg_str == "sha-256-sess" {
+                self.algorithm = DigestAlgorithm::SHA256;
+                self.session = true;
+            }
+        } else if key == "qop" {
+            let qop_str = val_pos.to_str(auth).to_lowercase();
+            if qop_str.contains(QualityOfProtection::AuthInt.to_str()) {
+                self.qop = QualityOfProtection::AuthInt;
+            } else {
+                self.qop = QualityOfProtection::Auth;
+            }
+        } else if key == "userhash" && val_pos.to_str(auth).to_lowercase() == "true" {
+            self.userhash = true;
+        }
+    }
+
     fn realm(&self) -> &str {
         self.realm.to_str(&self.www_authenticate)
     }
 
-    fn nonce(&self) -> &str {
+    pub fn nonce(&self) -> &str {
         self.nonce.to_str(&self.www_authenticate)
     }
 
@@ -399,19 +451,42 @@ impl DigestScheme {
 mod tests {
     #[test]
     fn rfc2069() {
-
         let rfc2069_test = r#"Digest realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41""#;
-        let mut d = rfc2069_test.parse::<crate::digest_authenticator::DigestScheme>().unwrap();
-        let auth_str = d.generate_auth_string("Mufasa", "CircleOfLife", "GET", "/dir/index.html", None, None);
-        assert_eq!(auth_str, r#"Digest username="Mufasa", realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", uri="/dir/index.html", response="1949323746fe6a43ef61f9606e7febea", opaque="5ccc069c403ebaf9f0171e9517f40e41""#);
+        let mut d = rfc2069_test
+            .parse::<crate::digest_authenticator::DigestScheme>()
+            .unwrap();
+        let auth_str = d.generate_auth_string(
+            "Mufasa",
+            "CircleOfLife",
+            "GET",
+            "/dir/index.html",
+            None,
+            None,
+        );
+        assert_eq!(
+            auth_str,
+            r#"Digest username="Mufasa", realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", uri="/dir/index.html", response="1949323746fe6a43ef61f9606e7febea", opaque="5ccc069c403ebaf9f0171e9517f40e41""#
+        );
     }
 
     #[test]
     fn rfc2617() {
         let rfc2617_test = r#"Digest realm="testrealm@host.com", qop="auth", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", opaque="5ccc069c403ebaf9f0171e9517f40e41""#;
-        let mut d = rfc2617_test.parse::<crate::digest_authenticator::DigestScheme>().unwrap();
-        let auth_str = d.generate_auth_string("Mufasa", "Circle Of Life", "GET", "/dir/index.html", None, Some("0a4f113b"));
-        assert_eq!(auth_str, r#"Digest username="Mufasa", realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", uri="/dir/index.html", qop=auth, algorithm=MD5, nc=00000001, cnonce="0a4f113b", response="6629fae49393a05397450978507c4ef1", opaque="5ccc069c403ebaf9f0171e9517f40e41""#);
+        let mut d = rfc2617_test
+            .parse::<crate::digest_authenticator::DigestScheme>()
+            .unwrap();
+        let auth_str = d.generate_auth_string(
+            "Mufasa",
+            "Circle Of Life",
+            "GET",
+            "/dir/index.html",
+            None,
+            Some("0a4f113b"),
+        );
+        assert_eq!(
+            auth_str,
+            r#"Digest username="Mufasa", realm="testrealm@host.com", nonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", uri="/dir/index.html", qop=auth, algorithm=MD5, nc=00000001, cnonce="0a4f113b", response="6629fae49393a05397450978507c4ef1", opaque="5ccc069c403ebaf9f0171e9517f40e41""#
+        );
     }
 
     #[test]
@@ -423,10 +498,21 @@ mod tests {
         nonce="7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v",
         opaque="FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS""#;
 
-        let mut d = rfc7617_test.parse::<crate::digest_authenticator::DigestScheme>().unwrap();
-        let auth_str = d.generate_auth_string("Mufasa", "Circle of Life", "GET", "/dir/index.html", None, Some("f2/wE4q74E6zIJEtWaHKaf5wv/H5QzzpXusqGemxURZJ"));
+        let mut d = rfc7617_test
+            .parse::<crate::digest_authenticator::DigestScheme>()
+            .unwrap();
+        let auth_str = d.generate_auth_string(
+            "Mufasa",
+            "Circle of Life",
+            "GET",
+            "/dir/index.html",
+            None,
+            Some("f2/wE4q74E6zIJEtWaHKaf5wv/H5QzzpXusqGemxURZJ"),
+        );
 
-        assert_eq!(auth_str, r#"Digest username="Mufasa", realm="http-auth@example.org", nonce="7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v", uri="/dir/index.html", qop=auth, algorithm=SHA-256, nc=00000001, cnonce="f2/wE4q74E6zIJEtWaHKaf5wv/H5QzzpXusqGemxURZJ", response="753927fa0e85d155564e2e272a28d1802ca10daf4496794697cf8db5856cb6c1", opaque="FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS""#);
+        assert_eq!(
+            auth_str,
+            r#"Digest username="Mufasa", realm="http-auth@example.org", nonce="7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v", uri="/dir/index.html", qop=auth, algorithm=SHA-256, nc=00000001, cnonce="f2/wE4q74E6zIJEtWaHKaf5wv/H5QzzpXusqGemxURZJ", response="753927fa0e85d155564e2e272a28d1802ca10daf4496794697cf8db5856cb6c1", opaque="FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS""#
+        );
     }
 }
-
